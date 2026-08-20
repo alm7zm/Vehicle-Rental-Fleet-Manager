9bc26040f847b239b37b027f3fabd5934703a910
Hussam Aldossary <h.s.m.d.23@gmail.com>
2026-08-20 17:16:48 +0300

    making the class Cloneable +setters and getter are truly added

--- src/main/java/maven/P/Reservation.java
+++ src/main/java/maven/P/Reservation.java
@@ -14,4 +14,40 @@ public class Reservation implements Cloneable {
 		this.days = days;
 	}
 
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	public String getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public int getDays() {
+		return days;
+	}
+
+	public void setDays(int days) {
+		this.days = days;
+	}
+
 }
